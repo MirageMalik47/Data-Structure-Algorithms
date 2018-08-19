6a50e6e48da804cbf5758de87b39f081f323dab3
Shubham Malik <41053014+shubhamak47@users.noreply.github.com>
2018-08-19 14:40:53 +0530

    Find nCr for given n and r

---
+++ Dynamic Programming/Binomial Coefficent.cpp
@@ -0,0 +1,42 @@
+//Binomial Coefficient - DP
+/* Find nCr for given n and r. */
+
+#include <bits/stdc++.h>
+using namespace std;
+//
+/*Recursive solution
+ int binomialCoeff(int n,int k)
+ {
+     if(n==k || k==0)
+         return 1;
+     else
+         return binomialCoeff(n-1,k-1)+binomialCoeff(n-1,k);
+ }
+*/
+
+// Tabular method - Bottom to top
+int binomialCoeff(int n,int k)
+{
+    if(n < k)
+    return 0;
+
+    int dp[n+1][k+1];
+    for(int i=0;i<=n ;i++)
+    {
+        for(int j=0;j<=k;j++)
+        {
+            if(i==j || j==0)
+             dp[i][j]=1;
+            else
+             dp[i][j]=( (dp[i-1][j-1]) + (dp[i-1][j]) ) % 1000000007;
+        }
+    }
+    return dp[n][k];
+}
+int main()
+{
+    int n,k;
+    cin>>n>>k;
+    cout<<binomialCoeff(n,k);
+    return 0;
+}
