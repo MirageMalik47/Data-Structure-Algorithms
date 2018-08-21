c3af64fb8bf37a4a124755deff1334fb84aa372b
Shubham Malik <41053014+shubhamak47@users.noreply.github.com>
2018-08-21 20:51:25 +0530

    Count ways to reach the Nth stair

--- Dynamic Programming/Ladders Problem.cpp
+++ Dynamic Programming/Ladders Problem.cpp
@@ -1,5 +1,6 @@
-/* The person can climb either 1 stair or 2 stairs at a time. 
-   Count the number of ways, the person can reach the top (order does matter  */
+/* There are n stairs, a person standing at the bottom wants to reach the top. 
+The person can climb either 1 stair or 2 stairs at a time. 
+Count the number of ways, the person can reach the top (order does matter).  */
    
 #include<bits/stdc++.h>
 using namespace std;
