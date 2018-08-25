66416955ecdcd9f48865579ae324cc1468b76f2a
Shubham Malik <41053014+shubhamak47@users.noreply.github.com>
2018-08-25 16:34:58 +0530

    Reverse Level Order Traversal

---
+++ Binary Tree/[]Imp] Reverse Level Order Traversal.cpp
@@ -0,0 +1,22 @@
+/* Time Comp : O(n) */
+void reversePrint(Node *root)
+{
+    queue<Node*> q;
+    q.push(root);
+    stack<Node*> st;
+    while(!q.empty())
+    {
+        Node* temp = q.front();
+        st.push(temp);
+        if(temp->right != NULL)
+            q.push(temp->right);
+        if(temp->left!=NULL)
+            q.push(temp->left);
+        q.pop();
+    }
+    while(!st.empty())
+    {
+        cout<<st.top()->data<<" ";
+        st.pop();
+    }
+}
