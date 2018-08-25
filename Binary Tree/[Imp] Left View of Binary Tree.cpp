c18eb58feb30acc0143c3a7ef606e14421a2daa5
Shubham Malik <41053014+shubhamak47@users.noreply.github.com>
2018-08-25 20:40:07 +0530

    Left View of Binary Tree

--- Binary Tree/[Imp] Left View of Binary Tree.cpp
+++ Binary Tree/[Imp] Left View of Binary Tree.cpp
@@ -1,4 +1,6 @@
 /* Time Comp : O(n) & Extra Space Required*/
+/* The left view contains all nodes that are first nodes in their levels. A simple solution is to do level order traversal and 
+print the first node in every level.*/
 
 void leftView(Node* root)
 {
