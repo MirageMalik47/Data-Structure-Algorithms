7f0238921332817dced2bada1e2afd43df2f9cf8
Shubham Malik <41053014+shubhamak47@users.noreply.github.com>
2018-08-25 15:51:33 +0530

    Count Non-Leaf Nodes in Tree

---
+++ Binary Tree/Count Non-Leaf Nodes in Tree .cpp
@@ -0,0 +1,11 @@
+/* Time Comp : O(n) */
+int countNonLeafNodes(Node* root)
+{
+   if(root == NULL)
+     return 0;
+     
+   if(root->left ==NULL && root->right ==NULL) 
+     return 0;
+    
+    return 1+ countNonLeafNodes(root->left) + countNonLeafNodes(root->right);
+}
