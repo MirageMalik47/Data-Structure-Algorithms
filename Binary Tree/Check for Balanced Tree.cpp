9c7dac6f2cc98b307b3803340897ace9b35db4d6
Shubham Malik <41053014+shubhamak47@users.noreply.github.com>
2018-08-25 11:01:09 +0530

    Check for Balanced Tree

---
+++ Binary Tree/Check for Balanced Tree.cpp
@@ -0,0 +1,23 @@
+/* A tree is height balanced if difference between heights of left and right subtrees is not more than one for all nodes of tree. */
+
+/* Time Comp : O(n^2) */
+
+int height(Node *root)
+{
+  if(root == NULL)
+    return 0; 
+  return max ( height(root->left) , height(root->right) ) + 1;
+}  
+bool isBalanced(Node *root)
+{
+  if(root == NULL)
+     return true;
+  
+   int lh = height(root->left);
+   int rh = height(root->right);
+  
+   if( abs(lh-rh) <= 1 &&  isBalanced(root->left) && isBalanced(root->right) )
+     return true;
+   else
+     return false;
+}
