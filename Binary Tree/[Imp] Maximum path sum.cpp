17b5fbea59cfb533c60d1a38a6218ec914aa5e53
Shubham Malik <41053014+shubhamak47@users.noreply.github.com>
2018-08-26 15:54:58 +0530

    Maximum Path Sum b/w two leaf of a Binary Tree

---
+++ Binary Tree/[Imp] Maximum path sum.cpp
@@ -0,0 +1,16 @@
+/* Time Comp : O(n) */
+int pathSum( Node* root, int &ans)
+{
+    if(root == NULL) 
+     return 0;
+    int l=pathSum(root->left, ans);
+    int r=pathSum(root->right, ans);
+    ans=max(ans,root->data+l+r);
+    return root->data + max(l,r);
+}
+int maxPathSum( Node *root)
+{
+    int ans=0;
+    int x=pathSum(root,ans);
+    return ans;
+}
