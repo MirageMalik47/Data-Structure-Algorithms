e133746c468271ff62dde108b57ca3fc5fb88afb
Shubham Malik <41053014+shubhamak47@users.noreply.github.com>
2018-08-26 13:12:04 +0530

    Top View of Binary Tree

---
+++ Binary Tree/[Imp] Top View of Binary Tree.cpp
@@ -0,0 +1,31 @@
+/* Time Comp : O(n) */
+void topView( Node *root)
+{
+    if (root == NULL)
+        return;
+ 
+    unordered_map<int, int> m;  //height_depth(hd) & Value;
+    queue<pair<Node*, int>> q;  // Node & hd;
+ 
+    q.push(make_pair(root, 0));
+    
+    while (!q.empty())
+    {
+        pair<Node*, int> p = q.front();
+        Node *n = p.first;
+        int hd = p.second;
+        q.pop();
+         
+        if ( m.count(hd) == 0 )   // if horizontal value is not in the hashmap that means it is the first value with tha    
+        {                          // horizontal distance so print it and store this value in hashmap
+            m[hd] = n->data;
+            cout<<n->data<<" ";
+        }
+         
+        if (n->left != NULL)
+            q.push(make_pair(n->left, hd-1));
+         
+        if (n->right != NULL)
+            q.push(make_pair(n->right, hd+1));
+    }
+}
