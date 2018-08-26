558517072b4f4895eceb7776bc9282107ff76bf4
Shubham Malik <41053014+shubhamak47@users.noreply.github.com>
2018-08-26 16:59:43 +0530

    Check if Given Tree is Height Balanced or Not

--- Binary Tree/[Imp] Symmetric Tree.cpp
+++ Binary Tree/[Imp] Symmetric Tree.cpp
@@ -1,9 +1,9 @@
 
 bool isMirror(Node *root1 , Node *root2)
 {
-    if( root1 == NULL && root2 == 0) return true;
-    if( root1 == NULL || root2 == 0) return false;
-    if( root1->key != root2->key) return false;
+    if( root1 == NULL && root2 == NULL) return true;
+    if( root1 == NULL || root2 == NULL) return false;
+    if( root1->data != root2->data) return false;
     return isMirror(root1->left,root2->right) && 
                         isMirror(root1->right,root2->left);
 }
