3fe97cb78c2b02a97421d0774f554f3cfce9cbaa
Shubham Malik <41053014+shubhamak47@users.noreply.github.com>
2018-08-26 12:38:58 +0530

    Vertical Order Traversal in Binary Tree

---
+++ Binary Tree/[Imp] Vertical Order Traversal in Binary Tree.cpp
@@ -0,0 +1,55 @@
+/* Interview Bit Solution */
+
+void getverticalorder(TreeNode *root , int hd ,map< int ,vector<int> >&m)
+{
+    if(root == NULL)
+     return ;
+
+     m[hd].push_back(root->val);
+     getverticalorder(root->left,hd-1,m);
+     getverticalorder(root->right,hd+1,m); 
+} 
+
+vector<vector<int> > Solution::verticalOrderTraversal(TreeNode* root) 
+{
+    map <int ,vector<int> > m;
+    vector< vector<int > > v;
+    int hd=0;
+    getverticalorder(root,hd,m);
+    
+    map< int,vector<int> > :: iterator it ;
+    for( it=m.begin() ; it!=m.end() ; it++)
+    {
+       v.push_back( it->second);
+    }
+    return v;
+}
+
+/* GeeksforGeeks Solution */
+
+void getverticalorder(Node *root , int hd ,map< int ,vector<int> >&m)
+{
+    if(root == NULL)
+     return ;
+
+     m[hd].push_back(root->data);
+     getverticalorder(root->left,hd-1,m);
+     getverticalorder(root->right,hd+1,m); 
+}
+
+void printVerticalOrder(Node* root)
+{
+    map <int ,vector<int> > m;
+    int hd=0;
+    getverticalorder(root,hd,m);
+
+    map< int,vector<int> > :: iterator it ;
+    for( it=m.begin() ; it!=m.end() ; it++)
+    {
+        for(int i=0; i < it->second.size() ; i++)
+            cout<<it->second[i]<<" ";
+        
+        cout<<endl;
+    }
+
+}
