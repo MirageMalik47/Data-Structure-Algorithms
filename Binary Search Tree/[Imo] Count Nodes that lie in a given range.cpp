be9bed8b4fb73983dc9016bf1881300cacbd21ff
Shubham Malik <41053014+shubhamak47@users.noreply.github.com>
2018-08-27 16:17:07 +0530

    Count BST nodes that lie in a given range

---
+++ Binary Search Tree/[Imo] Count Nodes that lie in a given range.cpp
@@ -0,0 +1,14 @@
+/******************* Time Comp : O(n) ***************/
+int getCountOfNode(Node *root, int l, int h)
+{
+   if(!root)
+    return 0;
+ 
+   if(root->data >= l && root->data <= h)
+    return 1 + getCountOfNode(root->left,l,h) + getCountOfNode(root->right,l,h);
+
+   if(root->data < l)
+    return getCountOfNode(root->right,l,h);
+   else if(root->data > h)    
+    return getCountOfNode(root->left,l,h);
+}
