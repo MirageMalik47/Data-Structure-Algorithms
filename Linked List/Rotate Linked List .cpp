e34aa6b131034a400f25c1306bf8edfe40b3adb6
Shubham Malik <41053014+shubhamak47@users.noreply.github.com>
2018-09-22 12:56:33 +0530

    Rotate Linked List

---
+++ Linked List/Rotate Linked List .cpp
@@ -0,0 +1,32 @@
+Node<int> *rotate( Node<int> *start, int p)
+{
+    int count=0;
+    Node<int> * y=start;
+    while(y!=NULL)
+    {
+        count++;
+        y=y->next;
+    }
+    if(count == p)
+     return start;
+    else
+    {
+        p=count-p;   //Clockwise Direction  // Anti Clockwise (p=count-p) is not needed
+        Node<int> * temp=start;
+        Node<int> * extra;
+        Node<int> * x;
+        for(int i=1;i<p;i++)
+        {
+           temp=temp->next ;
+           extra=temp->next;
+        }
+        temp->next=NULL;
+        x=extra;
+        while(extra->next != NULL)
+        {
+         extra=extra->next;
+        }
+        extra->next=start;
+        return x;
+    }
+}
