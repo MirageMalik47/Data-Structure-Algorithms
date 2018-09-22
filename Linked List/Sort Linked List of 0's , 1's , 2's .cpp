787457b3b10ad4d5644de384d13fb9c3b2e1336a
Shubham Malik <41053014+shubhamak47@users.noreply.github.com>
2018-09-22 14:47:36 +0530

    Given a linked list of 0s, 1s and 2s, sort it.

---
+++ Linked List/Sort Linked List of 0's , 1's , 2's .cpp
@@ -0,0 +1,37 @@
+
+
+void sortList(Node *head)
+{
+   int count0=0,count1=0,count2=0;
+   Node* temp=head;
+   while(temp!=NULL)
+   {
+        if(temp->data==0)
+          count0++;
+        if(temp->data==1)
+          count1++; 
+        if(temp->data==2)
+          count2++;
+          
+        temp=temp->next;  
+   }
+   temp=head;
+   while(temp!=NULL)
+   {
+       while(count0--)
+       {
+           temp->data=0;
+           temp=temp->next;
+       }
+       while(count1--)
+       {
+           temp->data=1;
+           temp=temp->next;
+       }
+       while(count2--)
+       {
+           temp->data=2;
+           temp=temp->next;
+       }
+   }
+}
