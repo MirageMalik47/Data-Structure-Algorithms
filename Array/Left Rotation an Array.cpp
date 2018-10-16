1197bc33e6694d97198801db9a12a1717d6c168d
Shubham Malik <41053014+shubhamak47@users.noreply.github.com>
2018-10-16 15:29:31 +0530

    Reversal algorithm for Rotation of Array

--- Array/Left Rotation an Array.cpp
+++ Array/Left Rotation an Array.cpp
@@ -2,9 +2,11 @@
 Algorithm :
 
 rotate(arr[], d, n)
-reverse(arr[], 1, d) ;
-reverse(arr[], d + 1, n);
-reverse(arr[], 1, n);
+
+reverse(arr[], 0, d) ;
+reverse(arr[], d , n-1);
+reverse(arr[], 0, n-1);
+
 Let AB are the two parts of the input array where A = arr[0..d-1] and B = arr[d..n-1]. The idea of the algorithm is :
 Reverse A to get ArB, where Ar is reverse of A.
 Reverse B to get ArBr, where Br is reverse of B.
