c87ba6c1289e5e71875965c0a95b451afb36cbbd
Shubham Malik <41053014+shubhamak47@users.noreply.github.com>
2018-10-25 16:23:11 +0530

    Minimum number of coins that make a given value

--- Dynamic Programming/[Imp] Minimum Coins To Make  a Amount .cpp
+++ Dynamic Programming/[Imp] Minimum Coins To Make  a Amount .cpp
@@ -1,13 +1,13 @@
 /*
-Given a value V, if we want to make change for V cents, and we have infinite supply of each of C = { C1, C2, .. , Cm} valued coins, 
+Given a value V, if we want to make change for 'amount' cents, and we have infinite supply of each of C = { C1, C2, .. , Cm} valued coins, 
 what is the minimum number of coins to make the change?
 Examples:
 
-Input: coins[] = {25, 10, 5}, V = 30
+Input: coins[] = {25, 10, 5}, amount = 30
 Output: Minimum 2 coins required
 We can use one coin of 25 cents and one of 5 cents 
 
-Input: coins[] = {9, 6, 5, 1}, V = 11
+Input: coins[] = {9, 6, 5, 1}, amount = 11
 Output: Minimum 2 coins required
 We can use one coin of 6 cents and 1 coin of 5 cents
 
@@ -15,10 +15,7 @@ We can use one coin of 6 cents and 1 coin of 5 cents
 
 /******************************** Time Comp : O(n*amount) ************************/
 
-#include<bits/stdc++.h>
-using namespace std;
-
-int Min_Number_of_Coins(int *arr,int coins,int amount)
+int Min_Number_of_Coins(int *coins,int n,int amount)                         //coins is array having 'n' coins
 {
     int dp[amount+1];
     for(int i=0;i<=amount;i++)
@@ -28,30 +25,15 @@ int Min_Number_of_Coins(int *arr,int coins,int amount)
     
     for(int i=1 ; i<=amount ; i++)
     {
-        for(int j=0 ; j <coins ; j++)
+        for(int j=0 ; j <n ; j++)
         {
-            if( arr[j]<= i )
+            if( coins[j]<= i )
             { 
-                int remaining = dp[i - arr[j]] ;
+                int remaining = dp[i - coins[j]] ;
                 if(remaining != INT_MAX)
                   dp[i]=min( dp[i], remaining + 1);
             }
         }
     }
     return dp[amount];
-}
-
-int main()
-{
-     int amount,coins;
-     cin>>amount>>coins;
-	  
-     int *arr=new int [coins];
-     for(int i=0;i<coins;i++)
-      cin>>arr[i];
-	  
-     cout<<Min_Number_of_Coins(arr,coins,amount);
-     
-     delete []arr;
-     return 0;
 }
