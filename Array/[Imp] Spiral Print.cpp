384626d227c12653f3a31217914472fcf4229bd5
Shubham Malik <41053014+shubhamak47@users.noreply.github.com>
2018-11-10 22:57:35 +0530

    Spiral Print

--- Array/[Imp] Spiral Print.cpp
+++ Array/[Imp] Spiral Print.cpp
@@ -1,3 +1,35 @@
+
+/************** Time Comp : O(n) **********************/
+
+void Spirally_traversing_matrix(int **input,int row ,int col)
+{
+  int r=row,c=col; // c is col from end //  r is row from end 
+  int i,k=0,l=0; //k is row from start // l is col from start
+
+  while(k < r && l < c)
+  {
+    for(i = k; i < c; i++)
+      cout<<input[k][i]<<" ";
+    k++;
+    
+    for(i = k ; i < r ; i++)
+      cout<<input[i][c-1]<<" ";
+    c--;
+
+    for( i = c-1 ; i >= l && k < r ; i--)
+      cout<<input[r-1][i]<<" ";
+    r--;
+    
+    for( i = r-1; i >= k && l < c ; i--)
+       cout<<input[i][l]<<" ";
+    l++;      
+ 
+  }	
+}
+
+
+
+
 /************** Time Comp : O(n) **********************/
 
 void spiralPrint(int input[][1000], int row, int col)
@@ -35,3 +67,6 @@ void spiralPrint(int input[][1000], int row, int col)
     }    
   }	
 }
+
+
+
