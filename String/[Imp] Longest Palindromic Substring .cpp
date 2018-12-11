40da9ddc3327426eab81e59bf76700d098773ef0
Shubham Malik <41053014+shubhamak47@users.noreply.github.com>
2018-12-11 19:02:06 +0530

    Longest Palindromic Substring

--- String/[Imp] Longest Palindromic Substring .cpp
+++ String/[Imp] Longest Palindromic Substring .cpp
@@ -11,9 +11,8 @@ void Longest_Palindrome_string(string s)
 {
     int start=0,max=1;
     int low,high;
-    int len = s.size();
-    
-    for(int i=1;i<len;i++)
+
+    for(int i=1;i<s.size();i++)
     {
         low=i-1;  // Find the longest even length palindrome with center points as i-1 and i.
         high=i;   
@@ -27,10 +26,10 @@ void Longest_Palindrome_string(string s)
             --low;
             ++high;
         }
+     
         low=i-1;
         high=i+1;
         while(low>=0 && high<len && s[low]==s[high]) // Find the longest odd length palindrome with center point as i 
-        // 
         {
             if(max < high-low+1)
             {
@@ -39,9 +38,9 @@ void Longest_Palindrome_string(string s)
             }
             --low;
             ++high;
-        }
-        
+        }      
     }
+ 
     for(int i=start ;i<start+max; i++)
     {
         cout<<s[i];
