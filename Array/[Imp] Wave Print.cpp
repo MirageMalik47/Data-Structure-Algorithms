fe178c8b265a12cad2a1e33eb897767bef3b79ba
Shubham Malik <41053014+shubhamak47@users.noreply.github.com>
2019-01-01 23:52:39 +0530

    Wave Print

--- Array/[Imp] Wave Print.cpp
+++ Array/[Imp] Wave Print.cpp
@@ -1,11 +1,10 @@
-//Given a two dimensional n*m array, print the array in a sine wave order. i.e. print the first column top to bottom, 
-//next column bottom to top and so on.
+/*Given a two dimensional n*m array, print the array in a sine wave order. i.e. print the first column top to bottom, 
+next column bottom to top and so on.
 Input : 1  2  3  4 
         5  6  7  8 
-        9 10 11 12
-	
+        9 10 11 12	
 Output : 1 5 9 10 6 2 3 7 11 12 8 4
-
+*/
 
 /************************** Time Comp : O(n) *****************/
 
