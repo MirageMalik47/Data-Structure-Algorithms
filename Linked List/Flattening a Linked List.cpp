60eae98237a22e58b613aa298090f762370a3f01
Shubham Malik <41053014+MirageMalik47@users.noreply.github.com>
2019-06-08 15:35:40 +0530

    Flattening a Linked List.cpp

---
+++ Linked List/Flattening a Linked List.cpp
@@ -0,0 +1,40 @@
+
+
+
+
+/* Node structure  used in the program
+class Node{
+	int data;
+  Node * next;
+  Node * bottom ;
+}; */
+
+Node *merge(Node*a,Node*b)
+{
+    Node *ans;
+    if(a==NULL)
+      return b;
+    if(b==NULL)
+      return a;
+    
+    if(a->data < b->data)
+    {
+        ans=a;
+        ans->bottom=merge(a->bottom,b);
+    }
+    else
+    {
+        ans=b;
+        ans->bottom=merge(a,b->bottom);
+    }
+    return ans;
+}
+    
+    
+Node *flatten(Node *root)
+{
+   if(root==NULL ||root->next==NULL)
+     return root;
+     
+    return merge(root,flatten(root->next)); 
+}
