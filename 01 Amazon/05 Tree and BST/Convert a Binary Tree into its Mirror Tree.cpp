9ab425f0954b7f1e72c6f35eb5dfbcd3c0f29bc6
Shubham Malik <41053014+MirageMalik47@users.noreply.github.com>
2019-08-31 15:15:30 +0530

    Convert a Binary Tree into its Mirror Tree

---
+++ 01 Amazon/05 Tree and BST/Convert a Binary Tree into its Mirror Tree.cpp
@@ -0,0 +1,16 @@
+Problem Statement : https://www.geeksforgeeks.org/write-an-efficient-c-function-to-convert-a-tree-into-its-mirror-tree/
+
+/**************************** Time Comp : O(n) *****************************/
+
+void mirror(Node* root) 
+{
+    if(root == NULL)
+      return ;
+      
+    Node * temp=root->left;      //Swap Nodes 
+    root->left=root->right;
+    root->right=temp;
+    
+    mirror(root->right) ;
+    mirror(root->left) ;    
+}
