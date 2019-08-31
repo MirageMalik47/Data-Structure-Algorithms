7f09789ec60272229c576d7a3b1fd41d7152541f
Shubham Malik <41053014+MirageMalik47@users.noreply.github.com>
2019-08-31 13:15:14 +0530

    Bottom View of Binary Tree.cpp

---
+++ 01 Amazon/05 Tree and BST/04 Bottom View of Binary Tree.cpp
@@ -0,0 +1,33 @@
+/*********************** Time Comp : O(n) ***************************/
+
+void bottomView(Node *root)
+{
+  if (root == NULL)
+        return;
+ 
+    map<int, int> m;  //height_depth(hd) & Value;
+    queue<pair<Node*, int>> q;  // Node & hd;
+ 
+    q.push(make_pair(root, 0));
+    
+    while (!q.empty())
+    {
+        pair<Node*, int> p = q.front();
+        Node *n = p.first;
+        int hd = p.second;
+        q.pop();
+         
+        m[hd] = n->data; //Last hd vaule is Stored in hashmap ;
+ 
+        if (n->left != NULL)
+            q.push(make_pair(n->left, hd-1));
+         
+        if (n->right != NULL)
+            q.push(make_pair(n->right, hd+1));
+    }
+    map<int, int> :: iterator it;
+    for(it=m.begin();it!=m.end();it++)
+    {
+        cout<<it->second<<" ";
+    }
+}
