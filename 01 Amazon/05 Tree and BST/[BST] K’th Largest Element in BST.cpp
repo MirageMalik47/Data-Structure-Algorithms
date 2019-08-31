f150f82613d81b6bb72eb0fb89668276d86432c7
Shubham Malik <41053014+MirageMalik47@users.noreply.github.com>
2019-08-31 20:30:39 +0530

    K’th Largest Element in BST

---
+++ 01 Amazon/05 Tree and BST/[BST] K’th Largest Element in BST.cpp
@@ -0,0 +1,42 @@
+Problem Statement : https://www.geeksforgeeks.org/kth-largest-element-in-bst-when-modification-to-bst-is-not-allowed/
+
+/************************* Time Comp : O(h + k) *******************************/
+
+void kthLargestUtil(Node *root, int k, int &c) 
+{ 
+    if (root == NULL || c >= k) 
+        return; 
+    
+    kthLargestUtil(root->right, k, c);    //so that the largest element is visited first 
+    
+    if (++c == k) 
+    { 
+        cout << root->data; 
+        return; 
+    } 
+    kthLargestUtil(root->left, k, c); 
+} 
+  
+void kthLargest(Node *root, int k) 
+{ 
+    int c = 0; 
+    kthLargestUtil(root, k, c); 
+}
+
+/*********************** Time Comp : O(n) & Space Comp : O(n) **********************/
+
+void inorder(Node* root,vector<int> &v)
+{
+  if(root == NULL)
+    return ;
+  
+   inorder(root->right,v);
+   v.push_back(root->key);
+   inorder(root->left,v);  
+}
+void kthLargest(Node *root, int k)
+{
+  vector<int> v;
+  inorder(root,v);
+  cout<<v[k-1]<<endl;
+}
