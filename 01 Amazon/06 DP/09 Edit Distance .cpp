cd421e03c443dbfa224f109b3035037160f979ba
Shubham Malik <41053014+MirageMalik47@users.noreply.github.com>
2019-09-03 23:17:45 +0530

    Update 09 Edit Distance .cpp

--- 01 Amazon/06 DP/09 Edit Distance .cpp
+++ 01 Amazon/06 DP/09 Edit Distance .cpp
@@ -2,7 +2,7 @@ Pronblem Statement : https://www.geeksforgeeks.org/edit-distance-dp-5/
 
 /************************ Time Comp : O(mn) ***********************/
 
-int editDistDP(string str1, string str2, int m, int n) 
+int editDistDP(string s1, string s2, int m, int n) 
 {  
     int dp[m+1][n+1]; 
   
@@ -12,17 +12,12 @@ int editDistDP(string str1, string str2, int m, int n)
         { 
             if (i==0)                             // If first string is empty
                 dp[i][j] = j;  
-  
             else if (j==0)                       // If Second string is empty
-                dp[i][j] = i; 
-  
-            else if (str1[i-1] == str2[j-1])     // If last characters are same
+                dp[i][j] = i;  
+            else if (s1[i-1] == s2[j-1])     // If last characters are same
                 dp[i][j] = dp[i-1][j-1]; 
-
-            else                                  // If last characters are different   
-                dp[i][j] = 1 + min(dp[i][j-1],  // Insert 
-                                   dp[i-1][j],  // Remove 
-                                   dp[i-1][j-1]); // Replace 
+            else                                    
+                dp[i][j] = 1 + min(dp[i][j-1], dp[i-1][j], dp[i-1][j-1]);  
         } 
     }  
     return dp[m][n]; 
