97cd038a398730c6d22d1686cc12ee92a1a5df85
Shubham Malik <41053014+MirageMalik47@users.noreply.github.com>
2019-09-05 17:16:45 +0530

    Check if Linked List is Palindrome

---
+++ 01 Amazon/03 Linked List/11 Check if Linked List is Palindrome.cpp
@@ -0,0 +1,52 @@
+Problem Statement : https://www.geeksforgeeks.org/function-to-check-if-a-singly-linked-list-is-palindrome/
+
+/********************* Time Comp : O(n) and Space Comp : O(1) *********/
+
+Node* reverse(Node *head)
+{
+    if(head->next==NULL)
+      return head;
+     
+    Node *x=reverse(head->next);
+    head->next->next=head;
+    head->next=NULL;
+    return x;
+}
+
+bool isPalindrome(Node *head)
+{
+    Node *temp=head;
+    int count=0;
+    while(temp!=NULL)
+    {
+        temp=temp->next;
+        count++;
+    }
+    if(count==1)
+     return true;
+    
+     bool x;
+     temp=head;
+     for(int i=1;i<=count/2;i++)
+     {
+         temp=temp->next;
+     }
+     
+    Node *midrev=reverse(temp);
+
+    for(int i=1;i<=count/2;i++)
+    {
+        if(head->data==midrev->data)
+        {
+            head=head->next;
+            midrev=midrev->next;
+            x=true;
+        }
+        else
+        {
+            x=false;
+            return 0;
+        }
+    }
+    return x;
+}
