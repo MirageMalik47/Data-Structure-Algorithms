1edce9401d7b93c714980bf76cda0d46a599ea4f
Shubham Malik <41053014+MirageMalik47@users.noreply.github.com>
2019-09-05 17:41:35 +0530

    Remove duplicates from an unsorted linked list

---
+++ 01 Amazon/03 Linked List/16 Remove duplicates from an unsorted linked list.cpp
@@ -0,0 +1,25 @@
+Problem Statement : https://www.geeksforgeeks.org/remove-duplicates-from-an-unsorted-linked-list/
+
+/************************ Time Comp : O(n) *************/
+
+Node * removeDuplicates(Node *root)
+{
+  unordered_set<int> m ;
+  Node *curr = root ;
+  Node *prev = NULL ;
+  while(curr)
+  {
+    if(m.count(curr->data) == 1)
+    {
+        prev->next  = curr->next ;
+        free(curr) ;
+    }
+    else
+    {
+        m.insert(curr->data) ;
+        prev = curr ;
+    }
+    curr = curr->next ;
+   }
+   return root;
+}
