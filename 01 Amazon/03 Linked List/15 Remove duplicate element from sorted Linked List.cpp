1917726ae6978b05bdbd38bdb486f6ae14fcf478
Shubham Malik <41053014+MirageMalik47@users.noreply.github.com>
2019-09-05 17:39:18 +0530

    Remove duplicate element from sorted Linked List

---
+++ 01 Amazon/03 Linked List/15 Remove duplicate element from sorted Linked List.cpp
@@ -0,0 +1,25 @@
+Problem Statement : https://www.geeksforgeeks.org/remove-duplicates-from-a-sorted-linked-list/
+
+/************************ Time Comp : O(n) *****************************/
+
+Node *removeDuplicates(Node *head)
+{
+    Node *temp=head;
+    Node *tail;
+       
+    if(head == NULL)
+      return NULL;
+      
+    while(temp->next!=NULL)
+    {
+        if(temp->data==temp->next->data)
+        {
+            tail=temp->next->next;
+            free(temp->next);
+            temp->next=tail;
+        }
+        else
+            temp=temp->next;      
+    }
+    return head;  
+}
